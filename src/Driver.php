1bd32b4c7baa7aefc6d1c6884a76f5daacf943e6
Command_String <bmxmikey2@gmail.com>
2023-03-17 07:51:06 +0000

    Converted to use PSR7 interface

--- src/Driver.php
+++ src/Driver.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Ragnarok\Bifrost\Drivers\React;
 
+use HttpSoft\Message\Response;
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Ragnarok\Bifrost\DriverInterface;
-use Ragnarok\Bifrost\Request;
-use Ragnarok\Bifrost\Response;
 use React\Http\Browser;
 use React\Promise\ExtendedPromiseInterface;
 
@@ -18,18 +18,20 @@ class Driver implements DriverInterface
     ) {
     }
 
-    public function makeRequest(Request $request): ExtendedPromiseInterface
+    public function makeRequest(RequestInterface $request): ExtendedPromiseInterface
     {
         return $this->browser->request(
-            $request->getMethod()->value,
-            $request->getEndpoint()->getCompleteEndpoint(),
+            $request->getMethod(),
+            $request->getUri(),
             $request->getHeaders(),
             $request->getBody()
         )->then(function (ResponseInterface $response) {
             return new Response(
                 $response->getStatusCode(),
-                (string) $response->getBody(),
-                $response->getHeaders()
+                $response->getHeaders(),
+                $response->getBody(),
+                $response->getProtocolVersion(),
+                $response->getReasonPhrase()
             );
         });
     }
